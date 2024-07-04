5a28bcc4a65c43876a006b173c22e12525586727
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2024-07-03 23:33:32 -0300

    add page links

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Esta é a HomePage
+      <a href="/produtos" className="bg-blue-500">
+        Ir para produtos
+      </a>
     </main>
   );
 }
--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function Products() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Esta é a página de produtos
+      <p>Esta é a página de produtos</p>
+      <div className="flex flex-col gap-2">
+        {Array.from(Array(11)).map((_, i) => (
+          <a key={i} href={`/produtos/${i + 1}`} className="border-2">
+            Produto {i + 1}
+          </a>
+        ))}
+      </div>
     </main>
   );
 }
