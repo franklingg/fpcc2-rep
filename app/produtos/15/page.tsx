7ba368e1381202045d6849ea22b61f5a0be2ad91
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2024-07-08 20:33:58 -0300

    adiciona mais paginas

---
+++ app/produtos/15/page.tsx
@@ -0,0 +1,86 @@
+import Image from "next/image";
+import { promises as fs } from "fs";
+
+interface IProduct {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand: string;
+  sku: string;
+  weight: number;
+  dimensions: {
+    width: number;
+    height: number;
+    depth: number;
+  };
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: [
+    {
+      rating: number;
+      comment: string;
+      date: string;
+      reviewerName: string;
+      reviewerEmail: string;
+    },
+    {
+      rating: number;
+      comment: string;
+      date: string;
+      reviewerName: string;
+      reviewerEmail: string;
+    },
+    {
+      rating: number;
+      comment: string;
+      date: string;
+      reviewerName: string;
+      reviewerEmail: string;
+    }
+  ];
+  returnPolicy: string;
+  minimumOrderQuantity: number;
+  meta: {
+    createdAt: string;
+    updatedAt: string;
+    barcode: string;
+    qrCode: string;
+  };
+  images: string[];
+  thumbnail: string;
+}
+
+async function getData() {
+  const file = await fs.readFile(process.cwd() + "/app/data.json", "utf8");
+  const data = JSON.parse(file) as { products: Array<IProduct> };
+  return data.products.find((p) => p.id == 15)!;
+}
+
+export default async function Product() {
+  const product = await getData();
+  return (
+    <article className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>{product.title}</p>
+      <p>
+        {product.dimensions.width}cm X {product.dimensions.height}cm X{" "}
+        {product.dimensions.depth}cm{" "}
+      </p>
+      <p>{product.description}</p>
+      <p>R${product.price}</p>
+      <Image
+        src={product.images[0]}
+        alt={`${product.title} image`}
+        width={200}
+        height={200}
+        priority
+      />
+    </article>
+  );
+}
