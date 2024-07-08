31930d1c6bd5a0e3c9d893d246c13c7d47feb6c8
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2024-07-08 20:36:10 -0300

    corrige pagina de produtos

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -5,7 +5,7 @@ export default function Products() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>Esta é a página de produtos</p>
       <div className="flex flex-col gap-2">
-        {Array.from(Array(11)).map((_, i) => (
+        {Array.from(Array(20)).map((_, i) => (
           <a key={i} href={`/produtos/${i + 1}`} className="border-2">
             Produto {i + 1}
           </a>
